a9ffec9c2e66d0429fc62f91adb6a07f3de7e0bc
Orxan223 <or.xan.2018@bk.ru>
2021-06-12 13:21:38 +0400

    Update

--- teachers/urls.py
+++ teachers/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import TeacherListView
+from .views import TeacherListView, TeacherDetailView
 
 urlpatterns = [
     path('', TeacherListView.as_view(), name='teachers'),
-  
+    path('teacher/<int:pk>', TeacherDetailView.as_view(), name="teacher_detail"),
+
 
 ]
