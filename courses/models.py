e072f5419f6f924caf5eac1a34c7eeed868e5637
Orxan223 <or.xan.2018@bk.ru>
2021-07-02 22:54:15 +0400

    'update;

--- courses/models.py
+++ courses/models.py
@@ -1,15 +1,14 @@
-from django.db.models.deletion import CASCADE
 from django.db import models
+from django.db.models.deletion import CASCADE
 from teachers.models import Teacher
+from django.contrib.auth.models import User
 
-# Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=200,null=True,blank=True)
-    slug = models.SlugField(max_length=50,unique=True,null=True)
-    
+    name = models.CharField(max_length=50, null=True)
+    slug = models.SlugField(max_length=50, unique=True, null=True)
+
     def __str__(self):
         return self.name
-
 
 class Tag(models.Model):
     name = models.CharField(max_length=50, null=True)
@@ -17,19 +16,18 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
-
-
+    
 
 class Course(models.Model):
-    teacher = models.ForeignKey(Teacher,null=True,on_delete=CASCADE)
-    category= models.ForeignKey(Category,null=True,on_delete=models.DO_NOTHING)
-    name = models.CharField (max_length=200, unique=True)
-    tags = models.ManyToManyField(Tag, blank=True,null=True)
-    description =models.TextField(blank=True,null=True)
-    image = models.ImageField(blank=True,null=True)
-    date= models.DateTimeField(auto_now=True)
-    avaible =models.BooleanField(default=True)
-
+    teacher = models.ForeignKey(Teacher, null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, unique=True)
+    category = models.ForeignKey(Category, null=True, on_delete=models.DO_NOTHING)
+    tags = models.ManyToManyField(Tag, blank=True, null=True)    
+    students = models.ManyToManyField(User, blank=True, related_name='courses_joined')
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to="courses/%Y/%m/%d/", default="courses/default_course_image.jpg")
+    date = models.DateTimeField(auto_now=True)
+    available = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
